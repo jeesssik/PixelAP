2bdf0b3b504990eeda25b96cfa8975bf6dd60938
Jesica Rodriguez <jeesssik@gmail.com>
2017-11-09 11:38:57 -0300

    archivos del juego

---
+++ pixelart.js
@@ -0,0 +1,138 @@
+var myPixelDraw={
+
+		colorPicked:0,
+		cellColor: '#ecf0f1',
+		defaultCells: 30,
+		cooloring: false,
+
+
+			fns:{
+
+				calcSize: function(cant){
+					if (typeof cant === 'undefined'){
+						cant=myPixelDraw.defaultCells;
+						}
+					    var cant = cant * cant;
+				        var container = myPixelDraw.container;
+				        var containerWidth = container.width();
+
+				        container.empty();
+
+				        for (i = 0; i < cant; i++) {
+				          container.append('<div class="cell" draggable></div>');
+				        }
+
+				        var cell = $('.cell');
+				        var cellSize = containerWidth / Math.sqrt(cant);
+
+				        cell.width(cellSize);
+				        cell.height(cellSize);
+
+
+					
+				},
+
+				reSize: function() {
+			        $('#sizeit').on('click', function(){
+			        	var newSizeVal = $('#resize').val();
+			        	if(newSizeVal==0 || newSizeVal > 50){
+			        		alert("Ingresá un número entre 1 y 49");
+			        		var newSizeVal=defaultCells;
+			            	}
+			            	else if(isNaN(newSizeVal)){
+			            		alert("Ingresá sólo números");
+			            		var newSizeVal=defaultCells;
+			               	}
+			               	myPixelDraw.fns.calcSize(newSizeVal);
+			        });
+			      },
+			    detectMouseUp: function() {
+			        $('#container').on('mouseup', function(){
+			        	myPixelDraw.coloring=false;
+			        });
+			      },
+			    colorPalette: function() {
+			        $('#color-pick > *').each(function(a,b) {
+		            var clase = $(b).attr('class');
+		            $(b).css('background-color', clase);
+       				 });
+			      },
+			    pickColor: function() {
+			         $('#color-pick > div').on('click', function() {
+		              myPixelDraw.colorPicked = $(this).attr('class');
+		              $(this).parent().find('.select').removeClass("select");
+		              $(this).addClass("select");
+         			 });
+			      },
+			    colorIt: function() {
+			        $(document).on('mousedown', '#container .cell', function(e) {
+		              e.preventDefault();
+		              myPixelDraw.coloring = true;
+		              
+		              if (e.button == 2) {
+		                  $(this).css('background-color', myPixelDraw.cellColor);
+		                  
+		              } else {
+		                  $(this).css('background-color', myPixelDraw.colorPicked);
+		              }
+			        })
+			      },
+			    colorOnDrag: function() {
+			         $('#container').on('mousemove', function(e) {
+              if (myPixelDraw.coloring == true) {
+                  var x = e.clientX;
+                  var y = e.clientY;
+                  var colorDraggedTo = document.elementFromPoint(x, y);
+                  if ($(colorDraggedTo).hasClass('cell') && e.button != 2) {
+                      $(colorDraggedTo).css('background-color', myPixelDraw.colorPicked);
+                  } else if ($(colorDraggedTo).hasClass('cell') && e.button == 2) {
+                      $(colorDraggedTo).css('background-color', myPixelDraw.cellColor);
+		                  }
+		              }
+		          });
+			      },
+			    reset: function() {
+			        $('#reset').on('click', function(e){
+			        	$('.cell').css('background-color', myPixelDraw.cellColor);
+			        })
+			      },
+			    toggleBorders: function() {
+			    	$('#toggle-border').on('click', function() {
+              		$('.cell').toggleClass('no-border');
+			    	});
+			      },
+			    disableRightClick: function() {
+			        myPixelDraw.container.on('contextmenu', function() {
+              		return false;
+         			 })
+			      },
+			    grabImage: function() {
+			        $('#grab-it').on('click',function(c){
+			        	var container = document.getElementById('container');
+			        		html2canvas(container, {
+							  onrendered: function(canvas) {
+							    
+							    var img = canvas.toDataURL("image/png");
+							    
+							    window.open(img);
+  }
+});
+			        });
+			      },
+			    },
+			    
+			    init: function(container) {
+			        myPixelDraw.container = container;
+			        var fns = myPixelDraw.fns;
+			        for (var i = 0; i < Object.keys(fns).length; i++) {
+			            fns[Object.keys(fns)[i]]();
+			        }
+    }
+};
+
+
+	$(document).ready(function() {
+	    var container = $('#container');
+	    myPixelDraw.init(container);
+	});
+
